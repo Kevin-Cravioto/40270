938c53c92d26976565f5fdcf2bf3f0b96c08b590
Kevin-Cravioto <kevinkravi@gmail.com>
2026-05-07 15:22:08 -0600

    tarea calculadora

--- javascript/javascript-02.js
+++ javascript/javascript-02.js
@@ -1,51 +1,105 @@
 let botones = document.getElementsByTagName("button");
 let resultado = document.getElementById("resultado");
-let prm 
-let prm2
+let prm;
+let prm2;
+let operacion;
 
-for (const key in botones ) {
-    if (Object.prototype.hasOwnProperty.call( botones , key)) {
-    const boton = botones [key];
-    console.log(boton)
-    boton.addEventListener("click", pintar)
+for (const key in botones) {
+    if (Object.prototype.hasOwnProperty.call(botones, key)) {
+        const boton = botones[key];
+        console.log(boton);
+        boton.addEventListener("click", pintar);
     }
-} 
+}
 
 function pintar(e) {
-    //console.log(e.target.className)
-    resultado.value += e.target.innerText
+    let valor = e.target.innerText;
+    if (valor == "+") {
+        sumar(e);
+    }
+    else if (valor == "-") {
+        restar(e);
+    }
+    else if (valor == "x"){
+        multiplicar(e);
+    }
+    else if (valor == "/"){
+        dividir(e);
+    }
+    else if (valor == "=") {
+        igual();
+    }
+    else if (valor == "ac") {
+        limpiar(e)
+    }
+    else {
+        resultado.value += valor;
+    }
 }
 
-
-let operadores = document.getElementsByClassName("numeros")
-console.log(operadores)
+let operadores = document.getElementsByClassName("numeros");
+console.log(operadores);
 
 for (const key in operadores) {
+
     if (Object.prototype.hasOwnProperty.call(operadores, key)) {
-    const boton = operadores[key];
-    console.log(boton)
-    boton.addEventListener("click", pintar2)
+        const boton = operadores[key];
+        console.log(boton);
+        boton.addEventListener("click", pintar2);
     }
-}    
-
-function pintar2(e){
-    console.log(e.target.innerText)
 }
 
-function limpiar(){
+function pintar2(e) {
+    console.log(e.target.innerText);
+}
+
+function limpiar(e) {
     resultado.value = "";
+    prm = "";
+    prm2 = "";
+    operacion = "";
 }
 
-function sumar (e){
+function sumar(e) {
     prm = resultado.value;
     operacion = e.target.innerText;
     resultado.value = "";
-    e.target.style.backgroundColor = "red"
+    e.target.style.backgroundColor = "red";
 }
 
-function igual(){
-    let prm2 = resultado.value
+function restar(e) {
+    prm = resultado.value;
+    operacion = e.target.innerText;
+    resultado.value = "";
+    e.target.style.backgroundColor = "red";
+}
+
+function multiplicar(e) {
+    prm = resultado.value;
+    operacion = e.target.innerText;
+    resultado.value = "";
+    e.target.style.backgroundColor = "red";
+}
+
+function dividir(e) {
+    prm = resultado.value;
+    operacion = e.target.innerText;
+    resultado.value = "";
+    e.target.style.backgroundColor = "red";
+}
+
+function igual() {
+    prm2 = resultado.value;
     if (operacion == "+") {
-        resultado.value = parseInt(prm) + parseInt(prm2)
+        resultado.value = parseInt(prm) + parseInt(prm2);
+    }
+    if (operacion == "-") {
+        resultado.value = parseInt(prm) - parseInt(prm2);
+    }
+    if (operacion == "x") {
+        resultado.value = parseInt(prm) * parseInt(prm2);
+    }
+    if (operacion == "/") {
+        resultado.value = parseInt(prm) / parseInt(prm2);
     }
 }
